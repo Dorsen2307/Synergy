5d81f93132e5a95a7f6afee0a6cea8748651b01b
GameFather <musorschik2008@yandex.ru>
2025-03-09 19:53:08 +0500

    Добавил урок №6 задание 3

---
+++ task_6_3/even_numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Введите первое целое число: "))
+b = int(input("Введите второе целое число, больше первого: "))
+
+if a <= b:
+    for i in range(a, b+1):
+        if i % 2 == 0:
+            print(i, end=" ")
+else:
+    print(f"{a} НЕ меньше {b}!")
