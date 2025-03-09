5f5d4cf8eb63c70242e27983de8e41a044d60f01
GameFather <musorschik2008@yandex.ru>
2025-03-09 15:33:12 +0500

    Добавил урок №3 задание 2

---
+++ task_3_2/biology_test.py
@@ -0,0 +1,8 @@
+stage_1 = input("Введите 1 этап: ")
+stage_2 = input("Введите 2 этап: ")
+stage_3 = input("Введите 3 этап: ")
+stage_4 = input("Введите 4 этап: ")
+stage_5 = input("Введите 5 этап: ")
+stage_6 = input("Введите 6 этап: ")
+
+print("\nЭтапы развития человека:\n" + stage_1, stage_2, stage_3, stage_4, stage_5, stage_6, sep=" => ")
