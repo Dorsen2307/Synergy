ab409f655cef6f0f9afe6c2d21ab73bfec7eb833
GameFather <musorschik2008@yandex.ru>
2025-03-10 12:48:46 +0500

    Изменен урок №8 задание 2

--- task_8_2/modified_array.py
+++ task_8_2/modified_array.py
@@ -10,9 +10,26 @@ def modified_array_2(array):
 
     return modify_array
 
-n = int(input("Введите количество чисел: "))
+n = 0
+sum = 0
 
-arr = list(map(int, input("Введите числа через пробел: ").split()))
+while (n < 1) or (n > 100000):
+    n = int(input("Введите количество чисел: "))
+    if not(1 <= n <= 100000):
+        print("Значение должно быть от 1 до 100000!\n")
+
+while True:
+    arr = list(map(int, input("Введите числа через пробел: ").split()))
+
+    if len(arr) == n:
+        for i in range(len(arr)):
+            sum += arr[i]
+        if 1 <= sum <= 10e9:
+            break
+        else:
+            print("Значение должно быть от 1 до 10e9!\n")
+    else:
+        print("Неверное количество значений!")
 
 print("Исходный список:\n ", *arr)
 print("Измененный список по 1 методу:\n ", *modified_array_1(arr))
