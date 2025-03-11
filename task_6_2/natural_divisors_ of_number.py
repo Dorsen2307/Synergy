219222d9b3b80bf3842f37d78315c18d8fd1e009
GameFather <musorschik2008@yandex.ru>
2025-03-11 12:29:20 +0500

    Скорректирован урок №6 задание 2

--- task_6_2/natural_divisors_ of_number.py
+++ task_6_2/natural_divisors_ of_number.py
@@ -1,12 +1,16 @@
-x = int(input("Введите число: "))
+x = 0
+while not(0 < x <= 2e9):
+    x = int(input("Введите число: "))
+    if not (0 < x <= 2e9):
+        print("Число должно быть от 0 до 2e9!")
 
-if x <= 2e9:
-    i = 1
-    result = 0
-    while i * i <= x:
-        if x % i == 0 and i * i != x:
-            result += 2
-        elif x % i == 0 and i * i == x:
+i = 1
+result = 0
+while i * i <= x:
+    if x % i == 0:
+        if i * i == x:
             result += 1
-        i += 1
-    print(f"Количество натуральных делителей числа {x}: {result}")
+        else:
+            result += 2
+    i += 1
+print(f"Количество натуральных делителей числа {x}: {result}")
