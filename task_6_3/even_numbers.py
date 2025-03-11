7f6125e14f0d7a0e2cb1fcdb89e110ecb7fae19f
GameFather <musorschik2008@yandex.ru>
2025-03-11 12:33:53 +0500

    Скорректирован урок №6 задание 3

--- task_6_3/even_numbers.py
+++ task_6_3/even_numbers.py
@@ -2,7 +2,7 @@ a = int(input("Введите первое целое число: "))
 b = int(input("Введите второе целое число, больше первого: "))
 
 if a <= b:
-    for i in range(a, b+1):
+    for i in range(a, b + 1):
         if i % 2 == 0:
             print(i, end=" ")
 else:
