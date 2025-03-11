1dcf88a0e75aaa0e2bd656eac69b9bc175a9d99b
GameFather <musorschik2008@yandex.ru>
2025-03-11 13:44:02 +0500

    Скорректирован урок №9 задание 2

--- task_9_2/intersection_lists.py
+++ task_9_2/intersection_lists.py
@@ -1,27 +1,35 @@
-
 def count_common_numbers(lst1, lst2):
+    """Возвращает количество общих чисел в двух списках."""
     set1 = set(lst1)
     set2 = set(lst2)
 
     common_numbers = set1.intersection(set2)
     return len(common_numbers)
 
-n1 = 0
-n2 = 0
+def get_positive_integer(prompt, min_value, max_value):
+    """Запрашивает у пользователя ввод положительного целого числа в заданном диапазоне."""
+    while True:
+        value = int(input(prompt))
+        if min_value <= value <= max_value:
+            return value
+        else:
+            print(f"Число должно быть от {min_value} до {max_value}!\n")
 
-while not(1 <= n1 <= 100000):
-    n1 = int(input("Введите количество чисел в первом списке: "))
-    if not(1 <= n1 <= 100000):
-        print("Количество не должно превышать 100000!")
+def get_number_list(n):
+    """Запрашивает у пользователя ввод n целых чисел и возвращает список."""
+    numbers = []
+    for i in range(n):
+        while True:
+            number = int(input(f"Введите число {i + 1}: "))
+            numbers.append(number)
+            break
+    return numbers
 
-list1 = [int(input()) for _ in range(n1)]
+n1 = get_positive_integer("Введите количество чисел в первом списке: ", 1, 100000)
+list1 = get_number_list(n1)
 
-while not(1 <= n2 <= 100000):
-    n2 = int(input("Введите количество чисел во втором списке: "))
-    if not(1 <= n2 <= 100000):
-        print("Количество не должно превышать 100000!")
-
-list2 = [int(input()) for _ in range(n2)]
+n2 = get_positive_integer("Введите количество чисел во втором списке: ", 1, 100000)
+list2 = get_number_list(n2)
 
 res = count_common_numbers(list1, list2)
 
