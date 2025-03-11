397c569b150c94fd95e4ac289bf88fb25b4f83f5
GameFather <musorschik2008@yandex.ru>
2025-03-11 12:40:26 +0500

    Скорректирован урок №7 задание 2

--- task_7_2/all_spaces_are_one.py
+++ task_7_2/all_spaces_are_one.py
@@ -3,12 +3,10 @@ new_text = ""
 length_text = 1000
 
 if len(text) < length_text:
-    for i in range(len(text)):
-        if text[i] != " ":
-            new_text += text[i]
-        else:
-            if text[i + 1] != " ":
-                new_text += " "
+    new_text = " ".join(text.split())
 
-print(f"Исходный текст:\n {text}\n")
-print(f"Преобразованный текст:\n {new_text}")
+    print(f"Исходный текст:\n {text}\n")
+    print(f"Преобразованный текст:\n {new_text}")
+else:
+    print(f"Преобразованный текст превышает {length_text} символов.")
+
