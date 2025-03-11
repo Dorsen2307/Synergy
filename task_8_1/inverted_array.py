c4f51f7350f169025ecc0509053159e63641c6c8
GameFather <musorschik2008@yandex.ru>
2025-03-11 13:08:06 +0500

    Скорректирован урок №8 задание 2

--- task_8_1/inverted_array.py
+++ task_8_1/inverted_array.py
@@ -1,5 +1,5 @@
 n = 0
-k = 0
+k = True
 summ = 0
 numbers = []
 
@@ -10,7 +10,7 @@ while not(1 <= n <= 10000):
         print("Количество должно быть от 1  до 10000!\n")
 
 # запрос чисел и проверка их суммы
-while k == 0:
+while k:
     numbers = [int(input(f"Введите число {i + 1}: ")) for i in range(n)]
 
     if len(numbers) == n:
@@ -18,7 +18,7 @@ while k == 0:
             summ += numbers[i]
 
         if 1 <= summ <= 10 ** 9:
-            k = 1
+            k = False
         else:
             summ = 0
             print("Значение должно быть от 1 до 10e9!\n")
--- task_8_2/modified_array.py
+++ task_8_2/modified_array.py
@@ -1,40 +1,34 @@
-def modified_array_1(array):
+def modified_array(array):
+    """Сдвигает элементы массива, перемещая последний элемент в начало."""
     array.insert(0, array[-1])
     array.pop()
     return array
 
-def modified_array_2(array):
-    modify_array = []
-    for i in range(len(array)):
-        modify_array.append(array[len(array) - 1 - i // 2] if i % 2 == 0 else array[i // 2])
-
-    return modify_array
-
 n = 0
 summ = 0
-k = 0
 arr = []
 
+# запрос количества чисел
 while not(1 <= n <= 100000):
     n = int(input("Введите количество чисел: "))
     if not(1 <= n <= 100000):
         print("Значение должно быть от 1 до 100000!\n")
 
-while k == 0:
+# запрос чисел и проверка их суммы
+while True:
     arr = list(map(int, input("Введите числа через пробел: ").split()))
 
     if len(arr) == n:
         for i in range(len(arr)):
             summ += arr[i]
-        if 1 <= summ <= 10e9:
-            k = 1
+
+        if 1 <= summ <= 10 ** 9:
+            break # завершаем цикл, если сумма в пределах
         else:
-            k = 0
             summ = 0
             print("Значение должно быть от 1 до 10e9!\n")
     else:
         print("Неверное количество значений!")
 
 print("Исходный список:\n ", *arr)
-print("Измененный список по 1 методу:\n ", *modified_array_1(arr))
-print("Измененный список по 2 методу:\n ", *modified_array_2(arr))
+print("Измененный список по 1 методу:\n ", *modified_array(arr))
